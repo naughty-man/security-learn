00fc8086d0f8910b0ef425e99813fe971b43b663
naughty-man <2168153989@qq.com>
2019-11-19 17:29:05 +0800

    fix

--- src/main/java/com/jerrysong/securitylearn/controller/LoginController.java
+++ src/main/java/com/jerrysong/securitylearn/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.jerrysong.securitylearn.config;
+package com.jerrysong.securitylearn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
